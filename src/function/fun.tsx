5db3e6b6de83d11d35fa1a29dee00ca33cd5b73c
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-04-01 19:59:00 +0300

    auto test

---
+++ src/function/fun.tsx
@@ -0,0 +1,12 @@
+
+export const fun1 = (a:number,b:number) => {
+  return a + b
+}
+
+export const fun2 = (a:number,b:number) => {
+  return a * b
+}
+
+export const fun3 = (a:number,b:number) => {
+  return a + b + 4
+}
