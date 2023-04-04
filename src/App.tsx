7fafe67f60adc538268c72f5ea06b7144b7b1955
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-04-04 22:29:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Cost } from './components/costItem';
 
-function App() {
+export function App() {
   return (
     <div className="App">
-     
+      Hello
+      <Cost />
     </div>
   );
 }
 
-export default App;
